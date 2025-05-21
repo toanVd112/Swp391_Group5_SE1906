d9ac016e2900a11ec68c71cc86177116538b4d56
namfptu <fcpctk@gmai.com>
2025-05-21 20:53:25 +0700

    int a

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -15,6 +15,7 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic heredfdfdfdfsdsdsdsdvvvvvvvvvvvvvvv
+        int a;
     }
     
 }
